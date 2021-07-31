cc3d7c6ec7c95a51b6417765665d269b11a06a68
Ryan Maguire <ryan_maguire@student.uml.edu>
2021-07-30 20:26:03 -0400

    Final update for elementaryOS.

--- scripts/SetupAcerSlim5ElementaryOSHera.sh
+++ scripts/SetupAcerSlim5ElementaryOSHera.sh
@@ -43,14 +43,12 @@ yes | sudo apt-get install ./caprine_2.54.0_amd64.deb
 
 # Install nordvpn.
 wget https://repo.nordvpn.com/deb/nordvpn/debian/pool/main/nordvpn-release_1.0.0_all.deb
-yes | sudo apt-get update
 yes | sudo apt-get install ./nordvpn-release_1.0.0_all.deb
 yes | sudo apt-get update
 yes | sudo apt-get install nordvpn
 
 # Install zoom.
-wget https://zoom.us/client/latest/zoom_amd64.deb
-yes | sudo apt-get update
+wget https://zoom.us/client/latest/zoom_amd64.debZ
 yes | sudo apt-get install ./zoom_amd64.deb
 
 # Install tweaks
