cfb2d388e2776ddea5298a16d4024b2bd29ef102
ryanmaguire <ryan_maguire@student.uml.edu>
2021-08-08 17:02:48 -0400

    Updates.

--- scripts/SetupAcerSlim5ElementaryOSHera.sh
+++ scripts/SetupAcerSlim5ElementaryOSHera.sh
@@ -7,19 +7,9 @@
 # Install useful stuff.
 sudo apt-get install --yes build-essential wget curl git
 
-# Install signal. These are the comments from signal.
-
-# NOTE: These instructions only work for 64 bit Debian-based
-# Linux distributions such as Ubuntu, Mint etc.
-
-# 1. Install our official public software signing key
+# Install signal.
 wget -O- https://updates.signal.org/desktop/apt/keys.asc | sudo apt-key add -
-
-# 2. Add our repository to your list of repositories
-echo "deb [arch=amd64] https://updates.signal.org/desktop/apt xenial main" |\
-  sudo tee -a /etc/apt/sources.list.d/signal-xenial.list
-
-# 3. Update your package database and install signal
+echo "deb [arch=amd64] https://updates.signal.org/desktop/apt xenial main" | sudo tee -a /etc/apt/sources.list.d/signal-xenial.list
 sudo apt update && sudo apt install --yes signal-desktop
 
 # Install Caprine Facebook messenger for Molly.
