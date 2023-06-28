e00789c76d5203c5340b033f6488742e534697e1
ryanmaguire <rmaguire@protonmail.com>
2023-06-27 21:32:22 -0400

    Updating stuff.

--- scripts/setup_projects.sh
+++ scripts/setup_projects.sh
@@ -1,8 +1,19 @@
 # Clone repos.
 mkdir -p $HOME/Documents/Code/
 cd $HOME/Documents/Code/
-git clone https://github.com/ryanmaguire/Mathematics-and-Physics.git
+git clone https://github.com/ryanmaguire/complex_visual_plots.git
+git clone https://github.com/ryanmaguire/hitandmis.system.git
+git clone https://github.com/ryanmaguire/kissvg.git
+git clone https://github.com/ryanmaguire/libtmpidl.git
 git clone https://github.com/ryanmaguire/libtmpl.git
+git clone https://github.com/ryanmaguire/libtmppl.git
+git clone https://github.com/ryanmaguire/libtmpyl.git
 git clone https://github.com/ryanmaguire/LinuxSetupScripts.git
+git clone https://github.com/ryanmaguire/Mathematics-and-Physics.git
+git clone https://github.com/ryanmaguire/mandelbrot_set.git
+git clone https://github.com/ryanmaguire/newton_fractals.git
+git clone https://github.com/ryanmaguire/newtonian_black_holes.git
+git clone https://github.com/ryanmaguire/ryanmaguire.github.io.git
+git clone https://github.com/ryanmaguire/spynr.git
 git clone https://github.com/NASA-Planetary-Science/rss_ringoccs.git
 git config --global credential.helper store
