d8df0939e510b472441d67faa8881f1a9a278b13
ryanmaguire <rmaguire@protonmail.com>
2026-03-17 15:24:22 -0400

    Updates.

--- scripts/setup_jstest.sh
+++ scripts/setup_jstest.sh
@@ -15,5 +15,5 @@
 #   You should have received a copy of the GNU General Public License          #
 #   along with this file.  If not, see <https://www.gnu.org/licenses/>.        #
 ################################################################################
-sudo apt install --yes jstest-gtk
+sudo apt-get install --yes jstest-gtk
 sudo apt update
